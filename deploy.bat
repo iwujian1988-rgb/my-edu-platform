90f3329cb2ca090fe36d6f6ad6d9ea642cc64ea6
XiaoYu Team <dev@xiaoyu.com>
2026-01-27 09:53:05 +0800

    fix: 更新部署脚本使用 --webpack 标志

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
-ssh root@43.99.58.240 "cd /root/my-edu-platform && git pull && npm run build && pm2 restart my-edu-platform"
+ssh root@43.99.58.240 "cd /root/my-edu-platform && git pull && npx next build --webpack && pm2 restart my-edu-platform"
 pause
